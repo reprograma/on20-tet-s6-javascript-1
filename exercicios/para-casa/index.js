73edb30d4c11a151185d6d731e7209b11b3c4a73
Caroline Meira <caroline.meira987@gmail.com>
2022-09-22 19:35:43 -0300

    Resolução da atividade 6

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,70 @@
+    let filmes = [
+    {
+      nome: "Deadpool",
+      classificacaoEtaria: 18 
+    },
+    {
+      nome: "Your Name",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Kill Bill",
+      classificacaoEtaria: 18
+    },
+    {
+      nome: "Shrek",
+      classificacaoEtaria: 0 
+    },
+    {
+      nome: "Matrix ",
+      classificacaoEtaria: 14 
+    },
+    {
+      nome: "Você nem imagina",
+      classificacaoEtaria: 12
+    },
+    {
+      nome: "Brilho eterno de uma mente sem lembranças",
+      classificacaoEtaria: 14
+    },
+    {
+      nome: "Scott Pilgrim contra o mundo",
+      classificacaoEtaria: 12
+    },
+    {
+      nome: "Capitão Fantástico",
+      classificacaoEtaria: 14
+    },
+    {
+      nome: "El Camino",
+      classificacaoEtaria: 16 
+    },
+    {
+      nome: "Parasita",
+      classificacaoEtaria: 16 
+    },
+    {
+      nome: "Star Wars: o retorno do jedi",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Red: crescer é uma fera",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Ninja Assassino",
+      classificacaoEtaria: 18
+    },
+    
+    ]
+
+    let idade = Number (prompt("Qual é a sua idade?"))
+        console.log(idade)
+
+
+    filmes.forEach(list =>{
+        if (idade >= list ["classificacaoEtaria"]){
+            document.write(list["nome"], "<br>")
+            console.log(list["nome"])
+        }
+     })
