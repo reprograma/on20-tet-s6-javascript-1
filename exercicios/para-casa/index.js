d253fa03f4c688f000af90dd9e4ce2499391ceff
Roberta Silva Gonçalves <robertasigon@gmail.com>
2022-09-22 15:12:06 -0300

    Resolução para-casa

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,75 @@
+//criar uma função que receb a idade
+//de uma pessoa e diga quais filmes
+//aquela pessoa pode assistir.use como base o seguinte objeto:
+let filmes = [
+    {
+      nome: "Deadpool",
+      classificacaoEtaria: 18 
+    },
+    {
+      nome: "Your Name",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Kill Bill",
+      classificacaoEtaria: 18
+    },
+    {
+      nome: "Shrek",
+      classificacaoEtaria: 0 
+    },
+    {
+      nome: "Matrix ",
+      classificacaoEtaria: 14 
+    },
+    {
+      nome: "Você nem imagina",
+      classificacaoEtaria: 12
+    },
+    {
+      nome: "Brilho eterno de uma mente sem lembranças",
+      classificacaoEtaria: 14
+    },
+    {
+      nome: "Scott Pilgrim contra o mundo",
+      classificacaoEtaria: 12
+    },
+    {
+      nome: "Capitão Fantástico",
+      classificacaoEtaria: 14
+    },
+    {
+      nome: "El Camino",
+      classificacaoEtaria: 16 
+    },
+    {
+      nome: "Parasita",
+      classificacaoEtaria: 16 
+    },
+    {
+      nome: "Star Wars: o retorno do jedi",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Red: crescer é uma fera",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Ninja Assassino",
+      classificacaoEtaria: 18
+    },
+    
+  ]
+
+  let idade = prompt("Qual é a sua idade")
+
+  filmes.forEach ((item) => {
+    if (idade < item.classificacaoEtaria) {
+        return console.log (`O filme ${item.nome} é inapropriado para a sua idade`)
+    }
+    else {
+        return console.log (`livre para assistir ${item.nome} !`)
+    }
+    }
+
+  )
