a477042fefa75ef5b38b78d015094ed2cd718956
Pati Rocha <patirocha.dev@gmail.com>
2022-09-26 15:57:59 -0300

    Atualizando

--- exercicios/para-casa/pati-rocha/script.js
+++ exercicios/para-casa/pati-rocha/script.js
@@ -64,7 +64,7 @@ let filmes = [
 
 let idade = Number(prompt("Digite sua idade e veja quais filmes você pode assistir:"));
 
-function faixaEtaria(item) {
+/*function faixaEtaria(item) {
   if (idade < 12){
     return item.classificacaoEtaria === 0
   } else if (idade >= 12 && idade < 14) {
@@ -79,9 +79,24 @@ function faixaEtaria(item) {
 }
 let filmesPorIdade = filmes.filter(faixaEtaria);
 
-console.log(filmesPorIdade);
+console.log(filmesPorIdade);*/
 
 
+// console.log(filmesPorIdade);
+
+
+
+
+ /*let exibir = filmes.filter((item) => {
+  if(idade < item.classificacaoEtaria) {
+  return console.log(`O filme ${item.nome} é inapropriado para sua idade!`)
+}
+else{
+  return console.log(`Livre para assistir ${item.nome} !`)
+}
+
+})
+console.log(exibir); */
 
 
 
